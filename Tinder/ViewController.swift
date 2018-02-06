4991f24a12250e1840eebd7cf341d54a5edcf4e1
homahi <homahi0128@gmail.com>
2018-02-06 17:29:58 +0900

    No13 画像が合わせて動くように修正

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -13,12 +13,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var likeImageView: UIImageView!
     @IBOutlet weak var basicCard: UIView!
+    @IBOutlet weak var person1: UIView!
+    @IBOutlet weak var person2: UIView!
+    @IBOutlet weak var person3: UIView!
+    @IBOutlet weak var person4: UIView!
     
     var centerOfCard: CGPoint!
+    
+    var people = [UIView]()
+    var selectedCardCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         centerOfCard = basicCard.center
+        people.append(person1)
+        people.append(person2)
+        people.append(person3)
+        people.append(person4)
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,11 +42,12 @@ class ViewController: UIViewController {
         let point = sender.translation(in: view)
         
         card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
+        people[selectedCardCount].center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
         
         // 角度を変える
         let xFromCenter = card.center.x - view.center.x
         card.transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2 ) * -0.785)
-        
+        people[selectedCardCount].transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2 ) * -0.785)
         if xFromCenter > 0 {
             likeImageView.image = #imageLiteral(resourceName: "good")
             likeImageView.alpha = 1
