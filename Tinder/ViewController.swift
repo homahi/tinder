aa3bc243c1e97433eaaa9537e0c0b8545de9f207
原野誉大 <harano@haranohomaredainoMacBook-puro.local>
2018-02-06 14:54:29 +0900

    スワイプスると傾く処理を追加

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -19,20 +19,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         centerOfCard = basicCard.center
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
     @IBAction func swipeCard(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
         let point = sender.translation(in: view)
         
         card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
         
+        // 角度を変える
+        let xFromCenter = card.center.x - view.center.x
+        card.transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2 ) * -0.785)
+        
+
+        if sender.state == UIGestureRecognizerState.ended {
+            
+            // 左に大きくスワイプ
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    card.center = CGPoint(x: card.center.x - 250, y: card.center.y)
+                })
+                return
+            } else if card.center.x > self.view.frame.width - 75 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    card.center = CGPoint(x: card.center.x + 250, y: card.center.y)
+                })
+                return
+                
+            }
+            
+            // 元に戻る処理
+            UIView.animate(withDuration: 0.2, animations: {
+                card.center = self.centerOfCard
+                card.transform = .identity
+            })
+        }
         
     }
+    
 }
 
